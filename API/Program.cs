04c3b765fad36fd43b6da519199c16a680f20890
Savalan11 <savalan.suleymanli@gmail.com>
2023-09-12 15:25:30 +0200

    end of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:61247"));
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:63528"));
 
 app.UseHttpsRedirection();
 
